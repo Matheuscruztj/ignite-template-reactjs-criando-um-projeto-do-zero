b52f6de4edc08f4dd6de8d81d1c837225fb7e623
Matheus Santos <matheus.cbmdk@gmail.com>
2023-03-31 18:29:38 -0300

    feat: dev - pt1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from 'next';
 
+import Link from 'next/link';
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import { useEffect, useState } from 'react';
 import { getPrismicClient } from '../services/prismic';
 
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { formatDate } from '../utils/dateFormat';
 
 interface Post {
   uid?: string;
@@ -22,15 +25,111 @@ interface PostPagination {
 
 interface HomeProps {
   postsPagination: PostPagination;
+  preview: boolean;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function Home({ postsPagination, preview }: HomeProps) {
+  const [posts, setPosts] = useState(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+  async function loadNextPage(link?: string) {
+    if (!link) {
+      return;
+    }
 
-//   // TODO
-// };
+    await fetch(link)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const postsResponse = data?.results.map(post => {
+          const { title, subtitle, author } = post.data;
+
+          return {
+            uid: post.uid,
+            first_publication_date: post.first_publication_date,
+            data: {
+              title,
+              subtitle,
+              author,
+            },
+          };
+        });
+
+        setPosts(posts);
+        setNextPage(data?.next_page);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  return (
+    <>
+      <head>Posts</head>
+      <div className={styles.contentContainer}>
+        <img className={styles.logo} src="/images/logo.svg" alt="news" />
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link
+              className={styles.post}
+              key={post.uid}
+              href={`/posts/${post.uid}`}
+            >
+              <a>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <div className={styles['tag-info-main']}>
+                  <div className={styles['tag-info']}>
+                    <FiCalendar className="icon" color="#04d361" />
+                    <p>{formatDate(post.first_publication_date)}</p>
+                  </div>
+                  <div className={styles['tag-info']}>
+                    <FiUser className="icon" color="#04d361" />
+                    <p>{post.data.author}</p>
+                  </div>
+                </div>
+              </a>
+            </Link>
+          ))}
+        </div>
+        <div className={styles.btnLoad} onClick={() => loadNextPage(nextPage)}>
+          Carregar mais posts
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
+  const prismic = getPrismicClient({});
+
+  const postsResponse = await prismic.getByType('posts', {
+    pageSize: 1,
+  });
+
+  const posts = postsResponse.results.map(post => {
+    const { title, subtitle, author } = post.data;
+
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title,
+        subtitle,
+        author,
+      },
+    };
+  });
+
+  return {
+    props: {
+      preview,
+      postsPagination: {
+        next_page: postsResponse?.next_page,
+        results: posts,
+      },
+    },
+  };
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -6,13 +6,13 @@ export interface PrismicConfig {
   req?: HttpRequestLike;
 }
 
-export function getPrismicClient(config: PrismicConfig): prismic.Client {
+export function getPrismicClient(config?: PrismicConfig): prismic.Client {
   const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
 
   enableAutoPreviews({
     client,
     req: config.req,
-  })
+  });
 
   return client;
 }
